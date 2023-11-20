fa1550c5aad3a0946542450d83f8a9567f546e2d
charansaiteja <saicharankommuri@gmail.com>
2023-11-20 01:39:13 -0500

    feat: added admin content history

--- evolved_vision/frontend/evolved-vision-ui/src/Components/admin/AdminHome.js
+++ evolved_vision/frontend/evolved-vision-ui/src/Components/admin/AdminHome.js
@@ -1,10 +1,75 @@
-import React from 'react';
-import './AdminHome.css'
+import React, { useState, useEffect } from "react";
+import "./AdminHome.css";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Heading,
+  Card,
+  CardBody,
+} from "@chakra-ui/react";
+import data from "../../testData/data.json";
 
 const AdminHome = () => {
-    return <>
-        <h1>Admin Home</h1>
-    </>
-}
+  const [contentData, setContentData] = useState(data.data);
+
+  useEffect(() => {
+    // fetch details
+  }, []);
+
+  const getTableHeaders = () => {
+    let tblHeader = <Th key={"headers-loading"}>LOADING...</Th>;
+    if (contentData) {
+      tblHeader = Object.keys(contentData[0]).map((key, index) => {
+        if (["description", "pdfSummary"].includes(key)) return;
+        return <Th key={index + "_" + key}>{key.toUpperCase()}</Th>;
+      });
+    }
+    return tblHeader;
+  };
+
+  const getTableRows = () => {
+    let tblRows = <Th key={"body-loading"}>LOADING...</Th>;
+    if (contentData) {
+      tblRows = contentData.map((row, index) => {
+        return (
+          <Tr key={index} style={{ cursor: "pointer" }}>
+            {Object.values(row).map((value, index) => {
+              if (![5, 6].includes(index)) return <Td key={index}>{value}</Td>;
+            })}
+          </Tr>
+        );
+      });
+    }
+    return tblRows;
+  };
+
+  return (
+    <div className="admin_table_container">
+      <div
+        className="table-button"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <Heading>Added Content History</Heading>
+      </div>
+      <Card variant={"elevated"}>
+        <CardBody>
+          <TableContainer>
+            <Table variant="striped" size={"md"}>
+              <Thead>
+                <Tr>{getTableHeaders()}</Tr>
+              </Thead>
+              <Tbody>{getTableRows()}</Tbody>
+            </Table>
+          </TableContainer>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
 
 export default AdminHome;
