656006da53475d0a0308d3fdf3a2e9624804cfe9
vinayv-456 <vinayvallabineni456@gmail.com>
2023-11-21 18:06:41 -0500

    feat: add session storage and protected routing

---
+++ evolved_vision/frontend/evolved-vision-ui/src/utils/ProtectedRoute.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { USER_ROLES } from "../constants";
+
+function ProtectedRoute() {
+  const user = JSON.parse(sessionStorage.getItem("user")) || {};
+
+  let loc = useLocation();
+  let location = loc.pathname;
+  if (user.id && location === "/") {
+    if (user.role === USER_ROLES.ADMIN) {
+      return <Navigate to="/admin" />;
+    } else {
+      return <Navigate to="/ar-view" />;
+    }
+  } else if (!user.id && location !== "/") {
+    // save the location so that after login user can be redirected to the same page
+    return <Navigate to="/" state={{ from: location }} />;
+  }
+  return <Outlet />;
+}
+
+export default ProtectedRoute;
