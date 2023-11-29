235f00d14aab940d49791c6049978ece455a47e9
vinayv-456 <vinayvallabineni456@gmail.com>
2023-11-29 15:32:49 -0500

    backend endpoint updation

--- evolved_vision/frontend/evolved-vision-ui/src/utils/APIService.js
+++ evolved_vision/frontend/evolved-vision-ui/src/utils/APIService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/";
+const API_URL = "http://44.219.170.153:8080/api/";
 export const TARGET_IMAGE_URL = "http://44.219.170.153:3001/generate-zpt";
 class APIService {
   postData(caller, operation, formData) {
