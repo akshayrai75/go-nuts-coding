5b59297a658d973a00106a92a01a81cedd8a8bc2
vinayv-456 <vinayvallabineni456@gmail.com>
2023-12-01 10:19:23 -0500

    add summary

--- evolved_vision/frontend/evolved-vision-ui/src/Components/ARContentDetails/index.js
+++ evolved_vision/frontend/evolved-vision-ui/src/Components/ARContentDetails/index.js
@@ -16,30 +16,37 @@ const ARContentDetails = (props) => {
     images,
     videos,
     orgTargetImage,
+    notes,
   } = location.state || {};
-  console.log("details", location, location.state);
+  console.log("details", notes, location, location.state);
   return (
-    <div className="mt-12 flex justify-around">
-      <div>
-        <span className=" font-bold text-3xl">Target Image</span>
-        <Image
-          className="mt-4"
-          style={{ maxWidth: "200px" }}
-          src={orgTargetImage}
-          alt="Original Target Image"
-          fluid
-        />
-      </div>
-      <div>
-        <div className="mb-4 font-bold text-3xl">Overlay</div>
+    <div>
+      <div className="mt-12 flex justify-around">
+        <div>
+          <span className=" font-bold text-3xl">Target Image</span>
+          <Image
+            className="mt-4"
+            style={{ maxWidth: "200px" }}
+            src={orgTargetImage}
+            alt="Original Target Image"
+            fluid
+          />
+        </div>
+        <div>
+          <div className="mb-4 font-bold text-3xl">Overlay</div>
 
-        {customTemplate ? (
-          <div className="froala-preview-content">
-            <FroalaEditorView model={modelAddress} />
-          </div>
-        ) : (
-          <div dangerouslySetInnerHTML={{ __html: modelAddress }} />
-        )}
+          {customTemplate ? (
+            <div className="froala-preview-content">
+              <FroalaEditorView model={modelAddress} />
+            </div>
+          ) : (
+            <div dangerouslySetInnerHTML={{ __html: modelAddress }} />
+          )}
+        </div>
+      </div>
+      <div className="p-6">
+        <div className=" font-bold text-3xl">Pdf summary</div>
+        <span className="">{notes}</span>
       </div>
     </div>
   );
